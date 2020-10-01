97a35d89ce366462c9d49e3b81e0e6d6e2dc9ad5
조성현 <>
2020-10-01 21:55:03 +0900

    데이터 내용 변경

--- src/store/state.js
+++ src/store/state.js
@@ -2,20 +2,20 @@ const menuState = {
   todoList: [
     {
       id:1,
-      title: "메뉴 설정",
-      content: "내용",
+      title: "달리기",
+      content: "10km 뛰기",
       status: false
     },
     {
       id:2,
-      title: "메뉴 설정22",
-      content: "내용",
-      status: false
+      title: "자전거 타기",
+      content: "ㅇㅇ",
+      status: true
     },
     {
       id:3,
-      title: "메뉴 설정33",
-      content: "내용33",
+      title: "공부하기",
+      content: "자바스크립트 공부",
       status: false
     },
   ]
